e8e3bbc18abcc7eb28b27d352799e31dec1a8099
Gustavo Ushjima <lgu85@hotmail.com>
2021-03-25 19:25:08 -0300

    initial features to add a new entry

---
+++ components/UdaciStepper.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {View, Text, TouchableOpacity} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome5'
+
+export default function UdaciStepper ({max, unit, step, value, onIncrement, onDecrement}) {
+    return (
+        <View>
+            <View>
+                <TouchableOpacity onPress={onDecrement}>
+                    <Icon name="minus" size={30} color={"black"} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={onIncrement}>
+                    <Icon name="plus" size={30} color={"black"} />
+                </TouchableOpacity>
+            </View>
+            <View>
+                <Text>{value}</Text>
+                <Text>{unit}</Text>
+            </View>
+        </View>
+    )
+}
