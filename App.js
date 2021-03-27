4b3ea53280daeec9230fd1c3b55c3cccaa883169
Gustavo Ushjima <lgu85@hotmail.com>
2021-03-27 11:34:51 -0300

    tab navigation

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler'
 import React, {Component} from 'react'
 import {Text, View} from 'react-native'
 
@@ -8,6 +9,15 @@ import reducer from './reducers'
 import AddEntry from './components/AddEntry'
 import History from './components/History'
 
+import {NavigationContainer} from '@react-navigation/native'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+import Teste from './components/Teste'
+import Live from './components/Live'
+
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+const Tabs = createBottomTabNavigator()
 export default class App extends Component {
   render() {
     const store = createStore(reducer)
@@ -15,7 +25,32 @@ export default class App extends Component {
     return (
       <Provider store={store}>
         <View style={{flex: 1}}>
-          <History />
+          <NavigationContainer>
+            <Tabs.Navigator
+              screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                  let iconName
+
+                  if (route.name === 'Teste') {
+                    iconName = focused
+                      ? 'ios-information-circle'
+                      : 'ios-information-circle-outline'
+                  } else if (route.name === 'Live') {
+                    iconName = focused ? 'ios-list-box' : 'ios-list'
+                  }
+
+                  // You can return any component that you like here!
+                  return <Ionicons name={iconName} size={size} color={color} />
+                },
+              })}
+              tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+              }}>
+              <Tabs.Screen name="Teste" component={Teste} />
+              <Tabs.Screen name="Live" component={Live} />
+            </Tabs.Navigator>
+          </NavigationContainer>
         </View>
       </Provider>
     )
